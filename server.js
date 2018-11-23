11724521dab335011dbd8939d94c08c9a20a7f34
Emanuel Stoeckli <emanuel.stoeckli@gmail.com>
2018-11-23 11:44:01 +0100

    Adjusting distribution

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
   app = express();
 app.use(express.static('./'));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 80);
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
